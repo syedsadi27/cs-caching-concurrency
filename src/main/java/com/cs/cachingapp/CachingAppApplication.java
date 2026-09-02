727906939beafeedcc66881ef27499c76da92a4c
Syed Sadi Ur Rahman <syed.r@cumulations.com>
2026-09-02 14:49:56 +0530

    Initial set-up

---
+++ src/main/java/com/cs/cachingapp/CachingAppApplication.java
@@ -0,0 +1,13 @@
+package com.cs.cachingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CachingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cs/cachingapp/CachingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs.cachingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
